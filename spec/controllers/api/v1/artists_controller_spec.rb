d908ef00f6050b69799ad0712d06a3243dfe1f01
R-Aranda <rupertaranda@gmail.com>
2022-08-03 11:21:11 -0400

    Fixed controller test for create Artist, PR comments finished

--- spec/controllers/api/v1/artists_controller_spec.rb
+++ spec/controllers/api/v1/artists_controller_spec.rb
@@ -14,6 +14,12 @@ RSpec.describe Api::V1::ArtistsController, type: :controller do
       bio: "The worst band...possibly ever! -Aidan"
     ) }
 
+  let!(:third_artist) {
+    Artist.create(
+      name: "The Luke Band",
+      bio: "Pretty Sick."
+    ) }
+
   describe "GET#index" do
     it "should return a list of all artists" do
       
@@ -33,36 +39,22 @@ RSpec.describe Api::V1::ArtistsController, type: :controller do
 
   describe "POST#create" do
     it "creates a new artist" do
-      post_json = {
-          artist: {
-              name: "The Luke Band",
-              bio: "Pretty Sick"
-          }
-      }
-
-      prev_count = Artist.count
-      post(:create, params: post_json, format: :json)
-      expect(Artist.count).to eq(prev_count + 1)
+      post_json = { name: third_artist.name, bio: third_artist.bio}
+      count = Artist.count
+      post(:create, params: {artist: post_json})
+      expect(Artist.count).to eq(count + 1)
     end
 
-    it "returns the json of the newly posted artist" do
-      post_json = {
-        artist: {
-            name: "The Luke Band",
-            bio: "Pretty Sick"
-        }
-      }
-
-      post(:create, params: post_json, format: :json)
+    it "returns json of new artist" do
+      post_json = { name: third_artist.name, bio: third_artist.bio}
+      post(:create, params: {artist: post_json})
       returned_json = JSON.parse(response.body)
-
       expect(response.status).to eq 200
       expect(response.content_type).to eq("application/json")
-
       expect(returned_json).to be_kind_of(Hash)
       expect(returned_json).to_not be_kind_of(Array)
-      expect(returned_json["name"]).to eq "The Luke Band"
-      expect(returned_json["bio"]).to eq "Pretty Sick"
+      expect(returned_json["name"]).to eq third_artist.name
+      expect(returned_json["bio"]).to eq third_artist.bio
     end
   end
 end
