1f2f6e1da4d24e89049523c85725808b64b0d30e
R-Aranda <rupertaranda@gmail.com>
2022-08-09 14:20:23 -0400

    Addressed comments from PR

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,10 +1,5 @@
 class Api::V1::UsersController < ApiController
   def show
-    user = User.find(params[:id])
-    render json: user
-  end
-
-  def index
-    render json: User.all
+    render json: User.find(params[:id])
   end
 end
--- app/javascript/react/components/Users/UserPage.js
+++ app/javascript/react/components/Users/UserPage.js
@@ -1,36 +1,37 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react"
 
 const UserPage = (props) => {
-  const [user, setUser] = useState([]);
-  const [profilePhoto, setProfilePhoto] = useState([]);
+  const [user, setUser] = useState([])
+  const [profilePhoto, setProfilePhoto] = useState([])
 
-  let userId = props.match.params.id;
+  let userId = props.match.params.id
 
   const getUser = async () => {
     try {
-      const response = await fetch(`/api/v1/users/${userId}`);
+      const response = await fetch(`/api/v1/users/${userId}`)
       if (!response.ok) {
-        const errorMessage = `${response.status} (${response.statusText})`;
-        throw new Error(errorMessage);
+        const errorMessage = `${response.status} (${response.statusText})`
+        throw new Error(errorMessage)
       }
-      const userData = await response.json();
-      setUser(userData.user);
+      const userData = await response.json()
+      setUser(userData.user)
       setProfilePhoto(userData.user.profile_photo.url)
     } catch (err) {
-      console.log(err);
+      console.log(err)
     }
-  };
+  }
 
   useEffect(() => {
-    getUser();
-  }, []);
+    getUser()
+  }, [])
+
   return (
     <div>
       <img src={profilePhoto} alt="Profile Photo" />
       <h1>{user.username}</h1>
       <div>{user.email}</div>
     </div>
-  );
-};
+  )
+}
 
-export default UserPage;
+export default UserPage
