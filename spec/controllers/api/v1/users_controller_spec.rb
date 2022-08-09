6e5457939893672067e72ddf28b5e202335f157a
R-Aranda <rupertaranda@gmail.com>
2022-08-09 15:20:13 -0400

    Added controller test for user show page

---
+++ spec/controllers/api/v1/users_controller_spec.rb
@@ -0,0 +1,15 @@
+require "rails_helper"
+RSpec.describe Api::V1::UsersController, type: :controller do
+  
+  describe "GET#show" do 
+    it "should show user page" do
+      user = FactoryBot.create(:user)
+
+      get :show, params: {id: user.id}
+      return_json = JSON.parse(response.body)
+
+      expect(response.status).to eq 200
+      expect(return_json["user"]["email"]).to eq user.email
+    end
+  end
+end
--- spec/support/factory_bot.rb
+++ spec/support/factory_bot.rb
@@ -1,11 +1,9 @@
 require 'factory_bot'
 
 FactoryBot.define do
-  # num = rand()
   factory :user do
     sequence(:email) {|n| "user#{n}@email.com" }
     password { 'password' }
     password_confirmation { 'password' }
-    # role { 'member' }
   end
 end
