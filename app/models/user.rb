2fa3dac597e91366093fdc1c2094e91751e79863
luketaylor9000 <lukeandrewtaylor.lt@gmail.com>
2022-08-09 11:05:54 -0400

    Started working on file_uploading and s3

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  mount_uploader :profile_photo, ProfilePhotoUploader
+
   def admin?
     role == "admin"
   end
