a3a565e9ac31fb038aca5f6711d156d06146320a
R-Aranda <rupertaranda@gmail.com>
2022-08-12 09:54:04 -0400

    merge main into voting, working on rspec

--- spec/support/factory_bot.rb
+++ spec/support/factory_bot.rb
@@ -9,3 +9,20 @@ FactoryBot.define do
     profile_photo { fixture_file_upload( Rails.root + 'spec/fixtures/images/test-image.png', "image/png") }
   end
 end
+
+FactoryBot.define do
+  factory :artist do
+    name {"Band"}
+    bio {"a bio"}
+  end
+end
+
+FactoryBot.define do
+  factory :review do
+      rating {"⭐️⭐️⭐️"}
+      title {"test title"}
+      body {"test body"}
+      artist_id { Artist.first.id }
+      user_id { User.first.id }
+  end
+end
