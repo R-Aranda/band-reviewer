8cf9546ffc761743fd66b2e3487a5d59670097d8
R-Aranda <rupertaranda@gmail.com>
2022-08-12 10:11:38 -0400

    votes fixed

--- app/javascript/react/components/Reviews/ReviewTile.js
+++ app/javascript/react/components/Reviews/ReviewTile.js
@@ -55,9 +55,11 @@ const ReviewTile = ({ title, rating, body, date, reviewId, artistId }) => {
         }
       );
       const newVoteData = await response.json();
+
+      // debugger;
       setCount(newVoteData.total_votes.total);
     } catch (error) {
-      console.error(`Error in Fetch: ${error.message}`);
+      alert("You must be signed in to vote!");
     }
   };
 
