45401165176748d671dee226235b94fbc820c874
R-Aranda <rupertaranda@gmail.com>
2022-08-12 12:50:36 -0400

    added submit new artist button

--- app/javascript/react/components/Artists/ArtistsContainer.js
+++ app/javascript/react/components/Artists/ArtistsContainer.js
@@ -1,32 +1,33 @@
-import React, { useState, useEffect } from "react"
-import ArtistTile from "./ArtistTile"
+import React, { useState, useEffect, Fragment } from "react";
+import ArtistTile from "./ArtistTile";
+import { Link } from "react-router-dom";
 
 const ArtistsContainer = (props) => {
-  const [artists, setArtists] = useState([])
-  const [adminRole, setAdminRole] = useState(false)
+  const [artists, setArtists] = useState([]);
+  const [adminRole, setAdminRole] = useState(false);
 
   const getArtists = async () => {
     try {
-      const response = await fetch("/api/v1/artists")
+      const response = await fetch("/api/v1/artists");
       if (!response.ok) {
-        const errorMessage = `${response.status} (${response.statusText})`
-        throw new Error(errorMessage)
+        const errorMessage = `${response.status} (${response.statusText})`;
+        throw new Error(errorMessage);
       }
-      const artistData = await response.json()
-      setArtists(artistData.artists)
+      const artistData = await response.json();
+      setArtists(artistData.artists);
       if (artistData.user) {
         if (artistData.user.role === "admin") {
-          setAdminRole(true)
+          setAdminRole(true);
         }
       }
     } catch (err) {
-      console.log(err)
+      console.log(err);
     }
-  }
+  };
 
   useEffect(() => {
-    getArtists()
-  }, [])
+    getArtists();
+  }, []);
 
   const deleteArtist = async (id) => {
     try {
@@ -38,12 +39,12 @@ const ArtistsContainer = (props) => {
           Accept: "application/json",
         },
         body: JSON.stringify(),
-      })
-      setArtists(artists.filter((artist) => artist.id !== id))
+      });
+      setArtists(artists.filter((artist) => artist.id !== id));
     } catch (error) {
-      console.log(error)
+      console.log(error);
     }
-  }
+  };
 
   const artistArray = artists.map((artist) => {
     return (
@@ -56,14 +57,19 @@ const ArtistsContainer = (props) => {
         adminRole={adminRole}
         deleteArtist={deleteArtist}
       />
-    )
-  })
+    );
+  });
 
   return (
-    <div className="artist-index-container">
-      <div className="grid-x">{artistArray}</div>
-    </div>
-  )
-}
+    <Fragment>
+      <div className="artist-index-container">
+        <div className="grid-x">{artistArray}</div>
+      </div>
+      <a href="button" className="artist-new-button">
+        Select a New Artist
+      </a>
+    </Fragment>
+  );
+};
 
-export default ArtistsContainer
+export default ArtistsContainer;
