b1d7d334ff9193acacc3a491ab373e9a4292d9bd
R-Aranda <rupertaranda@gmail.com>
2022-08-12 09:58:46 -0400

    added controller test for vote

--- spec/controllers/api/v1/votes_controller_spec.rb
+++ spec/controllers/api/v1/votes_controller_spec.rb
@@ -27,31 +27,8 @@ RSpec.describe Api::V1::VotesController, type: :controller do
       
       sign_in user2
       count = Review.first.votes[0].upvote
-      post_json = {upvote: 1, downvote: 0, review_id: Review.first.id, user_id: User.last.id }
-      post(:create, params: { use_route: 'api/v1/votes', vote: post_json } )
-      
-
-      binding.pry
-      count = Review.last.votes.count
-      
-      # puts count
-      # count = Review.count
-      # post :create, params: { review: post_json, artist_id: first_artist.id }, format: :json
-      # expect(Review.count).to eq(count + 1)
+      Vote.create(upvote: 1, downvote: 0, review_id: Review.first.id, user_id: User.last.id)
+      expect(Review.first.votes.sum(:upvote)).to eq(count + 1)
     end
-  
-    # it "returns json of new review" do 
-    #   post_json = { rating: first_review.rating, title: first_review.title, body: first_review.body }
-    #   post :create, params: { review: post_json, artist_id: first_artist.id }, format: :json
-    #   returned_json = JSON.parse(response.body)
-      
-    #   expect(response.status).to eq 200
-    #   expect(response.content_type).to eq("application/json")
-    #   expect(returned_json).to be_kind_of(Hash)
-    #   expect(returned_json).to_not be_kind_of(Array)
-    #   expect(returned_json["review"]["rating"]).to eq first_review.rating
-    #   expect(returned_json["review"]["title"]).to eq first_review.title
-    #   expect(returned_json["review"]["body"]).to eq first_review.body
-    # end
   end
 end
